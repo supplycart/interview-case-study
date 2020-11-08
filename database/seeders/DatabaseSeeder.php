fbb015880be5e40844d4f6b9ed948541286ff2a8
Akif md nor <dev.akifmdnor@gmail.com>
2020-11-08 16:02:38 +0800

    initialize auth with tailwind initialize vue login with verify email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => 'user.'.Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
