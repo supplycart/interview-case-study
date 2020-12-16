7d5d7a09c9ec07fdcb24e1ed0e09da6435334307
Nigel Hing <nigelhing1@gmail.com>
2020-12-16 23:37:14 +0800

    [WIP]User see products after login

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductsController extends Controller
 {
     public function index()
     {
-        return view('products.index');
+        $products = Product::all();
+        return view('products.index')->with('products', $products);
     }     
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
     <h1>This is products page</h1>
+    @if(count($products) > 0)
+        @foreach($products as $product) 
+            <div>
+                <h3>{{ $product->name }}</h3>
+            </div>
+        @endforeach
+    @else 
+        <p>No products found</p>
+    @endif
 @endsection
