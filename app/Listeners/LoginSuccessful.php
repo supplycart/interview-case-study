9de6cfdb8ded9639fa559c403f8783e5be5edcb7
Nigel Hing <nigelhing1@gmail.com>
2020-12-25 15:28:43 +0800

    [FEAT] Add event listener for logging in and out

---
+++ app/Listeners/LoginSuccessful.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LoginSuccessful
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Login  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        activity()                                        
+            ->log('login');
+    }
+}
---
+++ app/Listeners/LogoutSuccessful.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Logout;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LogoutSuccessful
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Logout  $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+        activity()                                        
+            ->log('logout');
+    }
+}
