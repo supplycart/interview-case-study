fb58a1901740e450601bb2994b6f86586d6058f9
Nigel Hing <nigelhing1@gmail.com>
2020-12-26 14:02:25 +0800

    [FIX]Fix categories seeder

--- database/seeds/CategoriesSeeder.php
+++ database/seeds/CategoriesSeeder.php
@@ -11,7 +11,7 @@ class CategoriesSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('Categories')->insert([
+        DB::table('categories')->insert([
             [
                 'name' => 'watch',                           
             ],            
