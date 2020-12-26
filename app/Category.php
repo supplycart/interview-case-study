3f1809376c8249e63666a80733b0c6cf102d951f
Nigel Hing <nigelhing1@gmail.com>
2020-12-26 13:43:59 +0800

    [FIX]Specify table for category model for heroku

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    public $table = "categories";
     //
     public function productCategories() {
         return $this->hasMany('App\ProductCategory');
