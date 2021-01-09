b3490d960c88225f97a2448538b55460e9bbb91f
ivanlowe <ai_zrobot@siswa.um.edu.my>
2021-01-09 23:03:05 +0800

    - initial commit - implemented login/register/logout - implemented email verification

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="max-w-6xl mx-4 sm:px-6 lg:px-8">
+            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                <img align="center" src="https://supplycart.my/wp-content/uploads/2019/09/sc_logo_tm.png">
+            </div>
+
+            <div class="grid grid-cols-1">
+                <div v-if="canLogin" class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                    <!--Dashboard-->
+                    <div v-if="$page.props.user" href="/dashboard" class="p-6">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+                            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Check your dashboard!</div>
+                        </div>
+
+                        <button @click="goToDashboard" class="uppercase px-8 py-2 ml-12 mb-6 rounded bg-blue-600 text-blue-50 max-w-max shadow-sm hover:shadow-lg">Dashboard</button>
+                    </div>
+                    <!--Login/Register-->
+                    <template v-else>
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                            <div class="flex items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Already have an account?</div>
+                            </div>
+                            <button @click="goToLogin" class="uppercase px-8 py-2 ml-12 mb-6 rounded bg-blue-600 text-blue-50 max-w-max shadow-sm hover:shadow-lg">Login</button>
+
+                            <div class="flex items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">New to SupplyCart?</div>
+                            </div>
+                            <button v-if="canRegister" @click="goToRegister" class="uppercase px-8 py-2 ml-12 mb-6 rounded bg-blue-600 text-blue-50 max-w-max shadow-sm hover:shadow-lg">Register</button>
+                        </div>
+                    </template>
+                </div>
+            </div>
+
+            <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    Built with Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .bg-gray-100 {
+        background-color: #f7fafc;
+        background-color: rgba(247, 250, 252, var(--tw-bg-opacity));
+    }
+
+    .border-gray-200 {
+        border-color: #edf2f7;
+        border-color: rgba(237, 242, 247, var(--tw-border-opacity));
+    }
+
+    .text-gray-400 {
+        color: #cbd5e0;
+        color: rgba(203, 213, 224, var(--tw-text-opacity));
+    }
+
+    .text-gray-500 {
+        color: #a0aec0;
+        color: rgba(160, 174, 192, var(--tw-text-opacity));
+    }
+
+    .text-gray-600 {
+        color: #718096;
+        color: rgba(113, 128, 150, var(--tw-text-opacity));
+    }
+
+    .text-gray-700 {
+        color: #4a5568;
+        color: rgba(74, 85, 104, var(--tw-text-opacity));
+    }
+
+    .text-gray-900 {
+        color: #1a202c;
+        color: rgba(26, 32, 44, var(--tw-text-opacity));
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .dark\:bg-gray-800 {
+            background-color: #2d3748;
+            background-color: rgba(45, 55, 72, var(--tw-bg-opacity));
+        }
+
+        .dark\:bg-gray-900 {
+            background-color: #1a202c;
+            background-color: rgba(26, 32, 44, var(--tw-bg-opacity));
+        }
+
+        .dark\:border-gray-700 {
+            border-color: #4a5568;
+            border-color: rgba(74, 85, 104, var(--tw-border-opacity));
+        }
+
+        .dark\:text-white {
+            color: #fff;
+            color: rgba(255, 255, 255, var(--tw-text-opacity));
+        }
+
+        .dark\:text-gray-400 {
+            color: #cbd5e0;
+            color: rgba(203, 213, 224, var(--tw-text-opacity));
+        }
+    }
+</style>
+
+<script>
+    import Button from "@/Jetstream/Button";
+    export default {
+        components: {Button},
+        props: {
+            canLogin: Boolean,
+            canRegister: Boolean,
+            laravelVersion: String,
+            phpVersion: String,
+        },
+        methods: {
+            goToDashboard: function () {
+                location.href='/dashboard'
+            },
+            goToLogin: function () {
+                location.href='/login'
+            },
+            goToRegister: function () {
+                location.href='/register'
+            },
+        },
+    }
+</script>
