94515f611f02318b4120dbbe1ad1a28fe3b81cd5
Hui Cong <huicong@protonmail.com>
2021-02-20 14:41:16 +0800

    Setup vue

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
 Route::get('/{any}', function(){
         return view('landing');
 })->where('any', '.*');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
