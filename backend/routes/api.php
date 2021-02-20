22b59c4086934446e7aaa1ad47690d4d39ee4bc4
Hui Cong <huicong@protonmail.com>
2021-02-20 10:18:40 +0800

    Define api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::get('/products', [ProductController::class,'index']);
+Route::post('/upload-file', [ProductController::class, 'uploadFile']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('users/{user}', [UserController::class, 'show']);
+    Route::patch('users/{user}', [UserController::class, 'update']);
+    Route::get('users/{user}/orders',[UserController::class, 'showOrders']);
+    Route::patch('products/{product}/units/add', [ProductController::class, 'updateUnits']);
+    Route::patch('orders/{order}/deliver', [OrderController::class, 'deliverOrder']);
+    Route::resource('/orders', OrderController::class);
+    Route::resource('/products', ProductController::class)->except(['index','show']);
 });
