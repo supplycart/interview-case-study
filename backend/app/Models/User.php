9cd32c90490629d106aee4aee6c7935979e2cff5
Hui Cong <huicong@protonmail.com>
2021-02-20 09:14:45 +0800

    Setup laravel passport

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes, HasApiTokens;
 
     protected $fillable = [
         'name', 'email', 'password',
