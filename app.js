b55d592d85709552539f9f66a8915b68155dc762
Sharif <hafizuddinshariff@gmail.com>
2021-03-10 09:23:47 +0000

    Update PORT

--- app.js
+++ app.js
@@ -223,14 +223,8 @@ app.post('/checkout', function(req, res) {
 // Others
 // =============================================================================
 
-let port = process.env.PORT;
-
-if (port == null || port == "") {
-  port = 3000;
-}
-
-app.listen(3000, function () {
-    console.log("Server started on port 3000")
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server started")
 })
 
 // Function to check if correct user and password
