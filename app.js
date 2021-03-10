e861e291ead40ccc071661f606a7c9f81ff146a4
Sharif <hafizuddinshariff@gmail.com>
2021-03-10 09:15:20 +0000

    Changed port

--- app.js
+++ app.js
@@ -223,6 +223,12 @@ app.post('/checkout', function(req, res) {
 // Others
 // =============================================================================
 
+let port = process.env.PORT;
+
+if (port == null || port == "") {
+  port = 3000;
+}
+
 app.listen(3000, function () {
     console.log("Server started on port 3000")
 })
