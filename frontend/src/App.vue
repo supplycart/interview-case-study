96d3bba05caefdb773d82877a0bc07ab316f0763
Muhammad Azizi <azizi@hotmail.com.my>
2021-05-30 23:32:36 +0800

    Update first UI for the login page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col items-center justify-center mt-11 w-full p-2">
+    <input class="w-full max-w-md" type="text" placeholder="User ID" v-model="userid">
+    <input class="w-full max-w-md" type="password" placeholder="Password" v-model="password">
+    <div v-if="loginError" class=" text-red-700">{{ loginError }}</div>
+    <button class="mt-2" @click="buttonClicked">Login</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+  },
+  data: function() {
+    return {
+      userid: '',
+      password: '',
+
+      loginError: ''
+    }
+  },
+  props: {
+  },
+  methods: {
+    buttonClicked: function() {
+      this.loginError = '';
+      if (!this.userid || !this.password) {
+        this.loginError = 'Please enter your user id and password!';
+      } else {
+        console.log(`Send to API, user id: ${this.userid} and pass: ${this.password}`);
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
