4dd01161e21c4b3d28461b0299614dcbd65bebe1
Muhammad Azizi <azizi@hotmail.com.my>
2021-05-31 13:49:04 +0800

    Init register page

---
+++ frontend/src/pages/RegisterPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  props: {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
