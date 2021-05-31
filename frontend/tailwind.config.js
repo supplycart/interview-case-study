a974098a463aea98101e34b4e304d7371c593d59
Muhammad Azizi <azizi@hotmail.com.my>
2021-05-31 13:41:44 +0800

    Added z index negative value

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      zIndex: {
+        '-10': '-10'
+      }
+    }
   },
   variants: {
     extend: {},
