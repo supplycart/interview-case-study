9bd3d7e631cd86a6a69927dde157013ad7a81696
Muhammad Azizi <azizi@hotmail.com.my>
2021-05-31 13:48:34 +0800

    Check the path

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="flex justify-center p-3 text-xs md:text-sm">
+    <div v-if="path == '/' || path.toLowerCase() == '/register'" class="bg-gradient-to-b from-blue-300 fixed top-0 left-0 h-full w-full z-back"></div>
     <router-view></router-view>
   </div>
 </template>
@@ -8,9 +9,17 @@
 export default {
   components: {
   },
+  data: function() {
+    return {
+      path: ''
+    }
+  },
   props: {
   },
   methods: {
+  },
+  mounted() {
+    this.path = this.$route.path;
   }
 };
 </script>
