13d90590b39d499c50954b9b4369315044b24462
Muhammad Azizi <azizi@hotmail.com.my>
2021-05-31 13:41:22 +0800

    Used router instead

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="flex flex-col items-center justify-center mt-11 w-full p-2">
-    <input class="w-full max-w-md" type="text" placeholder="User ID" v-model="userid">
-    <input class="w-full max-w-md" type="password" placeholder="Password" v-model="password">
-    <div v-if="loginError" class=" text-red-700">{{ loginError }}</div>
-    <button class="mt-2" @click="buttonClicked">Login</button>
+  <div class="flex justify-center p-3 text-xs md:text-sm">
+    <router-view></router-view>
   </div>
 </template>
 
@@ -11,25 +8,9 @@
 export default {
   components: {
   },
-  data: function() {
-    return {
-      userid: '',
-      password: '',
-
-      loginError: ''
-    }
-  },
   props: {
   },
   methods: {
-    buttonClicked: function() {
-      this.loginError = '';
-      if (!this.userid || !this.password) {
-        this.loginError = 'Please enter your user id and password!';
-      } else {
-        console.log(`Send to API, user id: ${this.userid} and pass: ${this.password}`);
-      }
-    }
   }
 };
 </script>
