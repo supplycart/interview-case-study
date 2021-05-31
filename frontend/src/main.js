0e6478406078701277f2b561b7b960b8200cdb85
Muhammad Azizi <azizi@hotmail.com.my>
2021-05-31 13:40:57 +0800

    Added and init vue router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+
+import Login from './pages/LoginPage.vue'
 
 import './css/tailwind.css'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    { path: '/', component: Login, meta: { title: 'Login' }  }
+  ],
+  mode: 'history'
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+
+  next()
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
