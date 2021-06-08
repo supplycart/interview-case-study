e9e783e4067ad2a4de7ef053676c77c710f1d44a
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-08 15:27:48 +0800

    Added the config for border color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     extend: {
       zIndex: {
         'back': '-99999999'
+      },
+      borderColor: {
+        'top-only': 'black transparent transparent transparent',
+        'right-only': 'transparent black transparent transparent',
+        'bottom-only': 'transparent transparent black transparent',
+        'left-only': 'transparent transparent transparent black',
+        'spin': 'black black black transparent'
       }
     }
   },
