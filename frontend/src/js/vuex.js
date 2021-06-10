72988943df2d134ba1a5e034193bbef91446c9b1
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-11 02:36:02 +0800

    Added vuex file to handle all the state

---
+++ frontend/src/js/vuex.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    allProducts: [],
+    cart: [],
+
+    totalCart: 0,
+    totalPrice: 0,
+
+    showGradient: true
+  },
+  mutations: {
+    countTotal (state) {
+      let totalCount = state.cart.map(c => Object.keys(c['sizes']).reduce((a, b) => { return a + c['sizes'][b] }, 0));
+      state.totalCart = totalCount.reduce((a, b) => { return a + b }, 0);
+      
+      state.totalPrice = 0;
+      totalCount.forEach((c, i) => state.totalPrice += (state.cart[i]['product']['price'] * c));
+    }
+  }
+})
