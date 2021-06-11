646a9738845b0f402f69119d60a8d43d97779847
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-12 03:36:32 +0800

    Added history and products page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,6 +11,8 @@ import VueX from './js/vuex.js';
 import Login from './pages/LoginPage.vue'
 import Register from './pages/RegisterPage.vue'
 import Home from './pages/HomePage.vue'
+import History from './pages/Home/HistoryPage.vue'
+import Product from './pages/Home/ProductPage.vue'
 
 import FormContainer from './components/FormContainer.vue'
 import Cart from './components/Cart.vue'
@@ -28,7 +30,12 @@ const router = new Router({
   routes: [
     { path: '/', component: Login, meta: { title: 'Login' }  },
     { path: '/Register', component: Register, meta: { title: 'Register' }  },
-    { path: '/Home', component: Home, meta: { title: 'Home' }  }
+    { path: '/Home', component: Home, meta: { title: 'Home' },
+      children: [
+        { path: '/', component: Product, meta: { title: 'Product' } },
+        { path: 'History', component: History, meta: { title: 'History' } }
+      ]
+    }
   ],
   mode: 'history'
 })
@@ -37,6 +44,7 @@ router.beforeEach((to, from, next) => {
   document.title = to.meta.title
 
   VueX.state.showGradient = document.title == 'Login' || document.title == 'Register'
+  VueX.state.gotoHomeChild = document.title == 'Product' ? 'Order History' : document.title == 'History' ? 'Products' : ''
 
   next()
 })
