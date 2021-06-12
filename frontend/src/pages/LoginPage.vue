ea491c1f1579d4e732131d627412dc98e2bae6c9
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-12 22:56:50 +0800

    Add loading, send activity when login success

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -5,7 +5,11 @@
     <label for="">Password</label>
     <input type="password" placeholder="********" v-model="password">
     <div v-if="loginError" class=" text-red-700">{{ loginError }}</div>
-    <button class="mt-2 w-full" @click="buttonClicked">Login</button>
+    <button class="mt-2 w-full" @click="loginClicked" :disabled="loading">Login</button>
+    <div v-if="loading" class="flex items-center justify-center mt-3">
+      <div class="border-spin rounded-full border-2 h-5 w-5 sm:h-7 sm:w-7 animate-spin"></div>
+      <div class="sm:text-sm font-bold ml-3">Logging in</div>
+    </div>
     <div class="mt-4 text-center w-full">
       Don't have an account?
       <span @click="registerClicked" class="cursor-pointer underline text-blue-400 select-none">Register</span>
@@ -22,24 +26,28 @@ export default {
       userid: '',
       password: '',
 
-      loginError: ''
+      loginError: '',
+      loading: false
     }
   },
   props: {
   },
   methods: {
-    buttonClicked: async function() {
+    loginClicked: async function() {
       this.loginError = '';
       if (!this.userid || !this.password) {
         this.loginError = 'Please enter your user id and password!';
       } else {
+        this.loading = true;
         let login = await this.$axios.post('/Users/Login', {
           'userID': this.userid,
           'password': this.password
         });
+        this.loading = false;
 
         if (login.data) {
           this.$cookies.set('user', login.data);
+          this.$activity.send('Login');
           this.$router.push('/Home');
         } else {
           this.loginError = 'User ID or Password is incorrect';
