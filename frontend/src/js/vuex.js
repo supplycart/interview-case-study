0ebd9f92ece7a498ef1080563e69ee719a3e5b61
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-13 00:40:54 +0800

    Added discount and add the total based on the discount

--- frontend/src/js/vuex.js
+++ frontend/src/js/vuex.js
@@ -13,6 +13,8 @@ export default new Vuex.Store({
 
     showGradient: true,
 
+    discount: 1, // 1 = no discount, 0.85 = 15% discount
+
     historyText: '',
     activityText: '',
 
@@ -24,7 +26,7 @@ export default new Vuex.Store({
       state.totalCart = totalCount.reduce((a, b) => { return a + b }, 0);
       
       state.totalPrice = 0;
-      totalCount.forEach((c, i) => state.totalPrice += (state.cart[i]['product']['price'] * c));
+      totalCount.forEach((c, i) => state.totalPrice += ((state.cart[i]['product']['price'] * state.discount) * c));
     }
   }
 })
