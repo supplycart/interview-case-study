a4ef01e4d9bc7f93774e42edd7bfe8fe0ba62c42
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-13 00:45:00 +0800

    Show the count of the activities

--- frontend/src/pages/Home/ActivityPage.vue
+++ frontend/src/pages/Home/ActivityPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="w-full p-3">
+    <div class="sm:text-2xl text-lg font-bold mb-3">Found {{ allActivities.length }} activities</div>
     <div v-if="loading" class="flex items-center">
       <div class="border-spin rounded-full border-2 h-5 w-5 sm:h-8 sm:w-8 animate-spin"></div>
       <div class="sm:text-xl font-bold ml-3">Getting Activities</div>
