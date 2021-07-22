665b8140090d4888e7e60350cb8f318fdc65e3a8
Marcus <mlim0032@student.monash.edu>
2021-07-22 15:04:13 +0800

    removed unnecessary code

--- supplycart/src/App.vue
+++ supplycart/src/App.vue
@@ -1,29 +1,14 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <div id="app">
     <router-view/>
-  <!-- <SignIn /> -->
   </div>
-  
-
-  <!-- <TopBar />
-  <Header />
-  <Products :products = "products"/> -->
 </template>
 
 <script>
-// import Header from './components/Header.vue'
-// import Products from './components/Products.vue'
-// import TopBar from './components/TopBar.vue'
-// import SignIn from './components/SignIn.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
-    // Header, Products, TopBar
-    // SignIn
   },
   data() {
     return {
@@ -45,27 +30,10 @@ export default {
       }
 
     ]
-      
     }
   }
 
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-
-/* *{
-  outline: red solid 1px;
-} */
-
-/* * {
-    margin: 0;
-} */
 </style>
