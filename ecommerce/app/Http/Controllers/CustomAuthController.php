67c8982463c02ff0b2bcc211fe0f1b9e64f9cbf1
kuanmy <kuan.mengyi@gmail.com>
2021-08-23 16:21:29 +0800

    Implement user login, registration and logout
    Reference: https://www.positronx.io/laravel-custom-authentication-login-and-registration-tutorial/

---
+++ ecommerce/app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class CustomAuthController extends Controller
+{
+    /**
+     * Get the view for login page.
+     */
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Validate login request. Redirect to dashboard page if
+     * login details are valid, else redirect back to login 
+     * page.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     */
+    public function customLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                            ->withSuccess('Signed in');
+        }
+
+        return redirect('login')->withSuccess('Login details are invalid');
+    }
+
+    /**
+     * Get the view for registration page.
+     */
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    /**
+     * Validate registration request. Redirect to dashboard page 
+     * if registration details are valid.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     */
+    public function customRegistration(Request $request)
+    {  
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+           
+        $data = $request->all();
+        $check = $this->create($data);
+         
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }   
+
+    
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }    
+    
+    /**
+     * Check authentication before redirecting to dashboard. If
+     * user is not logged in, redirect to login page.
+     */
+    public function dashboard()
+    {
+        if(Auth::check()) {
+            return view('dashboard');
+        }
+  
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+    
+    /**
+     * Sign out and redirect to login page.
+     */
+    public function signOut() {
+        Session::flush();
+        Auth::logout();
+  
+        return redirect('login');
+    }
+
+}
--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
