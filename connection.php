33bc55242e8a2af4cdc54cc90411604099f25cbb
SidOmIO <diksiddiq@gmail.com>
2022-05-19 20:12:20 +0800

    commit conn

--- connection.php
+++ connection.php
@@ -2,10 +2,18 @@
 
 function Connect()
 {
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpass = "";
-	$dbname = "homefood";
+
+	//local
+	//$dbhost = "localhost";
+	//$dbuser = "root";
+	//$dbpass = "";
+	//$dbname = "homefood";
+
+	//remote
+	$dbhost = "remotemysql.com";
+	$dbuser = "yezDtDbrzw";
+	$dbpass = "HzRLIwXRtB";
+	$dbname = "yezDtDbrzw";
 
 	//Create Connection
 	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
