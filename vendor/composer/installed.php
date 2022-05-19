7dc4f05aa6291cd80398bfc8f287488dacc4ce9a
SidOmIO <diksiddiq@gmail.com>
2022-05-19 20:48:37 +0800

    commit composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '33bc55242e8a2af4cdc54cc90411604099f25cbb',
+        'name' => 'vendor/name',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/name' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '33bc55242e8a2af4cdc54cc90411604099f25cbb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
