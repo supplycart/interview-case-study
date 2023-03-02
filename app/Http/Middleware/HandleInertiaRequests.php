ed375384f0bc0d1fa61325d46d042c9513e6f348
zhenquansim <s.zhenquan98@gmail.com>
2023-03-02 19:53:16 +0800

    Updated middleware and added assets

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,7 +39,7 @@ class HandleInertiaRequests extends Middleware
                     'location' => $request->url(),
                 ]);
             },
-            'auth.user.membership_level' => $request->user()->membership_level
+            'auth.user.membership_level' => optional($request->user())->membership_level
         ]);
     }
 }
