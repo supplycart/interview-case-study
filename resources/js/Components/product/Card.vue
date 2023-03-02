14b84fc09117186d73a18a21b299c7cb87f1c422
zhenquansim <s.zhenquan98@gmail.com>
2023-03-02 18:26:25 +0800

    Added inertia scaffold

---
+++ resources/js/Components/product/Card.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="rounded-lg border border-slate-200 p-2 flex flex-col">
+        <img
+            :src="product.image"
+            alt=""
+            class="aspect-square bg-contain rounded mb-2"
+        />
+        <p class="font-bold text-lg">{{ product.name }}</p>
+        <p class="grow text-sm">{{ product.description }}</p>
+        <p class="text-center mt-4 font-bold text-lg">
+            <span v-if="productDiscount > 0">
+                RM{{ discountedPrice.toFixed(2) }}
+                <s class="text-slate-400 text-sm ml-3">
+                    RM{{ product.price }}</s
+                >
+            </span>
+            <span v-else> RM{{ product.price.toFixed(2) }} </span>
+        </p>
+        <button class="bg-lime-200 w-full rounded-full py-1 mt-3 font-medium">
+            Add to Cart
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: { product: Object, productDiscount: Number },
+    computed: {
+        discountedPrice() {
+            return (
+                this.product.price -
+                this.product.price * (this.productDiscount / 100)
+            );
+        },
+    },
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +11,28 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::group(['middleware' => [
+    'auth', 'verified'
+]], function() {
+    Route::get('/', function () {
+        return redirect()->route('products.index');
+    });
+
+    Route::name('products.')->prefix('products')->group(function() {
+        Route::get('/', function() {
+            return Inertia::render('Product/List');
+        })->name('index');
+    });
+});
+require __DIR__.'/auth.php';
