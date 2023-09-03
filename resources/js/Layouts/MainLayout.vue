66d7ec5dd953b3a8a985728468e40c2ccf3e6037
cheryl-toh <cheryltqr2907@gmail.com>
2023-09-03 15:18:58 +0800

    first commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import { Link } from "@inertiajs/vue3";
+
+const showingNavigationDropdown = ref(false);
+</script>
+
+<template>
+    <div class="bg-red-300">cool</div>
+</template>
