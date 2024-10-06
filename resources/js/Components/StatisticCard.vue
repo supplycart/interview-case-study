db3c886e7b7db1ad93a50b0ab18d1987cec5cbdc
firdausnasir <firdaus.nasir@involve.asia>
2024-10-06 22:38:22 +0800

    remove dark theme text

--- resources/js/Components/StatisticCard.vue
+++ resources/js/Components/StatisticCard.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="space-y-2 w-full">
         <div
-            class="flex items-center space-x-2 rtl:space-x-reverse text-sm font-medium text-gray-500 dark:text-gray-400">
+            class="flex items-center space-x-2 rtl:space-x-reverse text-sm font-medium text-gray-600">
             <span>{{ name }}</span>
         </div>
 
-        <div class="text-3xl dark:text-gray-100">
+        <div class="text-3xl">
             {{ number }}
         </div>
     </div>
