e01bce475a66d9d7d6862a41c84544c7370d0e61
Casper Ho <casperyaoz@gmail.com>
2024-12-26 14:05:15 +0800

    Add seeder for product and product variations.

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $products = [
+            [
+                'name' => 'Product 1',
+                'sku' => 'SKU-1',
+                'description' => 'Product 1 description',
+                'variations' => [
+                    ['sku' => 'SKU-1-1', 'price' => 100],
+                    ['sku' => 'SKU-1-2', 'price' => 200],
+                    ['sku' => 'SKU-1-3', 'price' => 300],
+                ],
+            ],
+            [
+                'name' => 'Product 2',
+                'sku' => 'SKU-2',
+                'description' => 'Product 2 description',
+                'variations' => [
+                    ['sku' => 'SKU-2-1', 'price' => 100],
+                    ['sku' => 'SKU-2-2', 'price' => 200],
+                    ['sku' => 'SKU-2-3', 'price' => 300],
+                ],
+            ],
+            [
+                'name' => 'Product 3',
+                'sku' => 'SKU-3',
+                'description' => 'Product 3 description',
+                'variations' => [
+                    ['sku' => 'SKU-3-1', 'price' => 100],
+                    ['sku' => 'SKU-3-2', 'price' => 200],
+                    ['sku' => 'SKU-3-3', 'price' => 300],
+                ],
+            ],
+        ];
+
+        foreach ($products as $product) {
+            $productModel = Product::create([
+                'sku' => $product['sku'],
+            ], [
+                'name' => $product['name'],
+                'description' => $product['description'],
+            ]);
+
+            foreach ($product['variations'] as $variation) {
+                $productModel->variations()->create([
+                    'sku' => $variation['sku'],
+                ], [
+                    'price' => $variation['price'],
+                ]);
+            }
+        }
+    }
+}
