dee3ff7136e891ad14ce80a67c6c2a00982b053b
Casper Ho <casperyaoz@gmail.com>
2024-12-26 12:29:16 +0800

    Add model, migrations, controller and routes for product.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -24,4 +25,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::resource('products', ProductController::class)
+    ->only(['index', 'show'])
+    ->middleware(['auth']);
+
+require __DIR__ . '/auth.php';
