7f500a2d44950f2846cfbd89be697d02a245acf1
Casper Ho <casperyaoz@gmail.com>
2024-12-27 15:32:15 +0800

    Fix bug where HandleInertiaRequests.php throws error when user is unauthenticated.

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -32,7 +32,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $request->user(),
+                'user' => $request->user() ? $request->user()->only('name', 'email') : null,
             ],
         ];
     }
