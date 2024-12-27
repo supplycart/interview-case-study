34c8b8804d4d808adec78372579afea535c6a200
Casper Ho <casperyaoz@gmail.com>
2024-12-27 15:32:49 +0800

    Add vue devtools.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
     plugins: [
+        vueDevTools(),
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
