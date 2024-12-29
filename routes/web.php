142b46f7ea9430c6133424103b0e787becdb6a2f
Casper Ho <casperyaoz@gmail.com>
2024-12-29 21:10:33 +0800

    Update web.php to move product controller group into route group with `auth` middleware.

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::resource('products', ProductController::class)
-    ->only(['index', 'show'])
-    ->middleware(['auth']);
+    Route::resource('products', ProductController::class)
+    ->only(['index', 'show']);
+
+});
 
 require __DIR__ . '/auth.php';
