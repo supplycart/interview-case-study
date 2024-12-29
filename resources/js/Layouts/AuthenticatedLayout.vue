21ed3db1984d2bb1cb7aead9b8b00015355d7746
Casper Ho <casperyaoz@gmail.com>
2024-12-29 21:09:08 +0800

    Add cart icon in AuthenticatedLayout.vue's header section.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -46,7 +46,39 @@ const showingNavigationDropdown = ref(false);
                             </div>
                         </div>
 
-                        <div class="hidden sm:ms-6 sm:flex sm:items-center">
+                        <div
+                            class="hidden sm:ms-6 sm:flex sm:items-center"
+                            href="#"
+                        >
+                            <!-- Cart button -->
+                            <Link
+                                :href="route('cart.show')"
+                                class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                            >
+                                <svg
+                                    class="h-6 w-6"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 32 32"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        class="cls-1"
+                                        d="M29.46 10.14A2.94 2.94 0 0 0 27.1 9H10.22L8.76 6.35A2.67 2.67 0 0 0 6.41 5H3a1 1 0 0 0 0 2h3.41a.68.68 0 0 1 .6.31l1.65 3 .86 9.32a3.84 3.84 0 0 0 4 3.38h10.37a3.92 3.92 0 0 0 3.85-2.78l2.17-7.82a2.58 2.58 0 0 0-.45-2.27zM28 11.86l-2.17 7.83A1.93 1.93 0 0 1 23.89 21H13.48a1.89 1.89 0 0 1-2-1.56L10.73 11H27.1a1 1 0 0 1 .77.35.59.59 0 0 1 .13.51z"
+                                    />
+                                    <circle
+                                        class="cls-1"
+                                        cx="14"
+                                        cy="26"
+                                        r="2"
+                                    />
+                                    <circle
+                                        class="cls-1"
+                                        cx="24"
+                                        cy="26"
+                                        r="2"
+                                    />
+                                </svg>
+                            </Link>
                             <!-- Settings Dropdown -->
                             <div class="relative ms-3">
                                 <Dropdown align="right" width="48">
