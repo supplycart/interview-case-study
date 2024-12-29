a27b3a50d0046bff00aa43b04713cb18bd20fa19
Casper Ho <casperyaoz@gmail.com>
2024-12-29 12:45:55 +0800

    Update product variation factory to seed image urls that won't change everytime an image is requested.

--- database/factories/ProductVariationFactory.php
+++ database/factories/ProductVariationFactory.php
@@ -19,11 +19,12 @@ class ProductVariationFactory extends Factory
     {
 
         $this->faker->addProvider(new FakerPicsumImagesProvider($this->faker));
+        $randomSeed = $this->faker->words(1);
         return [
             'name' => $this->faker->safeColorName(), // Random color name
             'sku' => strtoupper($this->faker->unique()->lexify('VAR-???-????')), // Unique SKU
             'price' => $this->faker->randomFloat(2, 5, 100), // Random price between $5 and $100
-            'image' => $this->faker->imageUrl(width: 800, height: 800),
+            'image' => "https://picsum.photos/seed/$randomSeed[0]/800/800",
         ];
     }
 }
