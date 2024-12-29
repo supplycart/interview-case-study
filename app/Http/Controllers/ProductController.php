ef95cf5d5e90ab63e2f3622ab34715698abe4723
Casper Ho <casperyaoz@gmail.com>
2024-12-29 21:08:19 +0800

    Remove unused controller method stubs.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Products/Index', [
+        return Inertia::render('Product/Index', [
             'products' => Product::select('id', 'name')
                 ->with(['variations' => function ($query) {
                     $query->select('id', 'product_id', 'name', 'price', 'image')
@@ -24,52 +24,12 @@ class ProductController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(Product $product)
     {
-        return Inertia::render('Products/Show', [
+        return Inertia::render('Product/Show', [
             'product' => $product->load('variations'),
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Product $product)
-    {
-        //
     }
 }
