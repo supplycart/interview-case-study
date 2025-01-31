385494731692e0c56499ea097e194451ddcf2f6f
Nadzmi Idzham <nadzmiidzham@gmail.com>
2025-01-31 15:44:23 +0800

    added auth features

--- resources/js/Pages/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="flex justify-center items-center h-screen">
-        <h1 class="text-4xl font-bold">Hello World!</h1>
-    </div>
-</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,27 @@
-import "./bootstrap";
-import { createApp } from "vue";
+import '../css/app.css';
+import './bootstrap';
 
-import App from "./App.vue";
-import router from "./router";
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-createApp(App).use(router).mount("#app");
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
