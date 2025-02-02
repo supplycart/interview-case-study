b90512d553732aa09a075e76e1269cd4587b1b6f
Nadzmi Idzham <nadzmiidzham@gmail.com>
2025-02-02 17:31:40 +0800

    added missing import for ProductController class

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
