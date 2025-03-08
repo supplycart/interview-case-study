1ccd6fe8bad8b4e4be33248930f242ad0ef9d6a9
Reisfira <mohamadyuzrie@gmail.com>
2025-03-08 15:43:25 +0800

    feat: add logout and auth test function

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -15,5 +16,25 @@ class AuthController extends Controller
         $token = $request->user()->createToken('api-token');
 
         return ['access_token' => $token->plainTextToken];
+    }
+
+    public function logout(Request $request)
+    {
+        // delete all tokens, essentially logging the user out
+        auth()->user()->tokens()->delete();
+
+        // delete the current token that was used for the request
+        $request->user()->currentAccessToken()->delete();
+
+        return [ 'message' => 'Tokens and currentAccessToken deleted. Please login to obtain a new token.' ];
+    }
+
+    public function user(Request $request)
+    {
+        $data = [];
+        $data['request->user()'] = $request->user();
+        $data['auth()->user()'] = auth()->user();
+
+        return $data;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [ AuthController::class, 'login' ]);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('user', [ AuthController::class, 'user' ]);
+    Route::post('logout', [ AuthController::class, 'logout' ]);
+});
