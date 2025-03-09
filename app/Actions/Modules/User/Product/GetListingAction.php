d37b9394f20425e674b63c6644339085ae420f2a
Reisfira <mohamadyuzrie@gmail.com>
2025-03-09 16:54:03 +0800

    fix: update wrong object being used

--- app/Actions/Modules/User/Product/GetListingAction.php
+++ app/Actions/Modules/User/Product/GetListingAction.php
@@ -12,7 +12,7 @@ class GetListingAction
 
         foreach ($products as $product)
         {
-            $price = $user->is_member ? $user->discounted_price_for_member : $user->base_price;
+            $price = $user->is_member ? $product->discounted_price_for_member : $product->base_price;
             $product->price = number_format($price, 2);
         }
 
