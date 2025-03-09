943db8ed3283605d222924cb8e848f18fb4339e8
Reisfira <mohamadyuzrie@gmail.com>
2025-03-09 16:55:33 +0800

    feat: add product listing view with add to cart button

---
+++ resources/js/pages/user/product/Index.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { SharedData, type BreadcrumbItem } from '@/types';
+import { Head, useForm, usePage } from '@inertiajs/vue3';
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+interface Props {
+    products: any;
+    message?: string;
+}
+
+const props = defineProps<Props>();
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Product',
+        href: '/product',
+    },
+];
+
+function addToCart(product: any)
+{
+    let form = useForm({
+        product_id: product.id,
+        product_title: product.title,
+        product_description: product.description,
+        quantity: 1,
+        unit_price: product.price,
+    })
+
+    form.post(route('add-to-cart'));
+}
+
+</script>
+
+<template>
+    <Head title="Product" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Title</TableHead>
+                        <TableHead>Description</TableHead>
+                        <TableHead>Price</TableHead>
+                        <TableHead></TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow v-for="product in props.products.data">
+                        <TableCell class="font-medium">{{ product.title }}</TableCell>
+                        <TableCell>{{ product.description }}</TableCell>
+                        <TableCell>{{ product.price }}</TableCell>
+                        <TableCell>
+                            <Button @click="addToCart(product)">Add To Cart</Button>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
+    </AppLayout>
+</template>
