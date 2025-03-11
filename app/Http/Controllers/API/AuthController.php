b03327adde6937dee22ecbc2c8e287279c102cc7
Reisfira <mohamadyuzrie@gmail.com>
2025-03-11 15:07:48 +0800

    fix: update API/AuthController to use actions

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Actions\Modules\General\Authentication\GetAuthenticatedUserAction;
+use App\Actions\Modules\General\Authentication\LoginAction;
+use App\Actions\Modules\General\Authentication\LogoutAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
@@ -11,30 +14,22 @@ class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        $request->authenticate();
-
-        $token = $request->user()->createToken('api-token');
+        $token = LoginAction::execute($request);
 
         return ['access_token' => $token->plainTextToken];
     }
 
     public function logout(Request $request)
     {
-        // delete all tokens, essentially logging the user out
-        auth()->user()->tokens()->delete();
-
-        // delete the current token that was used for the request
-        $request->user()->currentAccessToken()->delete();
+        LogoutAction::execute($request);
 
         return [ 'message' => 'Tokens and currentAccessToken deleted. Please login to obtain a new token.' ];
     }
 
-    public function user(Request $request)
+    public function user()
     {
-        $data = [];
-        $data['request->user()'] = $request->user();
-        $data['auth()->user()'] = auth()->user();
+        $user = GetAuthenticatedUserAction::execute();
 
-        return $data;
+        return $user;
     }
 }
