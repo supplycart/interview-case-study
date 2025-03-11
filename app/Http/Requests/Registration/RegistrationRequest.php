0fa3d43f4eb84d820dfbfba95cb3d5eff71d3f6e
Reisfira <mohamadyuzrie@gmail.com>
2025-03-11 15:49:09 +0800

    fix: update registration API

--- app/Http/Requests/Registration/RegistrationRequest.php
+++ app/Http/Requests/Registration/RegistrationRequest.php
@@ -25,6 +25,11 @@ class RegistrationRequest extends FormRequest
                 'email',
                 'max:255',
             ],
+            'phone_no' => ['required','alpha_num:ascii','max:11'],
+            'address_line_1' => ['required','max:100'],
+            'address_line_2' => ['nullable','max:100'],
+            'address_line_3' => ['nullable','max:100'],
+            'is_member' => ['required', Rule::in(['yes', 'no']) ],
             'password' => ['required', 'string', 'max:255', 'confirmed'],
         ];
     }
