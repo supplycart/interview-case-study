02f1e4e86a656d0be9423f4d1f35521419e17e68
Reisfira <mohamadyuzrie@gmail.com>
2025-03-11 17:28:43 +0800

    feat: create simple database seeder and implement simple pagination (not from laravel)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,10 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Category::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $categories = Category::all();
+        foreach ($categories as $category)
+        {
+            Product::factory(10)->for($category)->create();
+        }
     }
 }
