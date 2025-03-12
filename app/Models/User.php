d3671f7799f6957599b7a1be696d85ea4d36c035
Reisfira <mohamadyuzrie@gmail.com>
2025-03-13 02:54:13 +0800

    chore: disable verify email due to smtp constraint on DigitalOcean

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
