5def020ae8cd0d8984551b852160a63199db520a
nasrulirfan <nasrulirfan02@gmail.com>
2025-05-12 00:01:30 +0800

    #{ISSUE_NUMBER} Fully working with bonus tasks (with tests) v2

--- backend/config/database.php
+++ backend/config/database.php
@@ -118,7 +118,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel_test'), // Use a separate test database name
+            'database' => env('DB_TEST_DATABASE', 'laravel_test'), // Use the specific test database env variable
             'username' => env('DB_USERNAME', 'sail'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
